ee170d04adf38161cfde27f5d9002f6547d45317
Daniel <doanhnoi@gmail.com>
2026-02-04 09:28:03 +0700

    chore: temporarily disable Bullet gem
    - Set Bullet.enable = false to disable N+1 query warnings
    - Disable alert, console, logger, and footer notifications
    - Will re-enable after fixing N+1 issues across the application

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
-# TASK-PERF: Enable Bullet for N+1 detection in development
-# To view Bullet warnings, check:
-# - Browser console
-# - Rails log
-# - Footer notification
-Bullet.enable = true
-Bullet.alert = true
-Bullet.bullet_logger = true
-Bullet.console = true
-Bullet.rails_logger = true
-Bullet.add_footer = true
+# TASK-PERF: Bullet for N+1 detection in development
+# Currently DISABLED - will enable after fixing N+1 issues
+# To re-enable: set Bullet.enable = true
+Bullet.enable = false
+Bullet.alert = false
+Bullet.bullet_logger = false
+Bullet.console = false
+Bullet.rails_logger = false
+Bullet.add_footer = false
 
 # Optional: Raise errors on N+1 (useful for strict development)
 # Bullet.raise = true
