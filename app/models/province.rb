9ace791ccb3bbc4ccea9060ad700e9505c571359
Daniel <doanhnoi@gmail.com>
2026-02-05 14:19:31 +0700

    Revert: remove auto-generate code from Province
    Will apply auto-generate code to Region instead per user request

--- app/models/province.rb
+++ app/models/province.rb
@@ -31,9 +31,6 @@ class Province < ApplicationRecord
   has_many :regions, through: :province_regions
   has_many :contacts, dependent: :nullify
 
-  # Callbacks - Auto-generate code from name
-  before_validation :generate_code_from_name, if: -> { name.present? && (code.blank? || name_changed?) }
-
   # Validations
   validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
   validates :code, presence: true, uniqueness: true, length: { maximum: 20 },
@@ -47,37 +44,5 @@ class Province < ApplicationRecord
   # Get region names for display
   def region_names
     regions.pluck(:name).join(", ")
-  end
-
-  private
-
-  # Generate code from Vietnamese name
-  # Example: "Thành phố Hà Nội" -> "THANH_PHO_HA_NOI"
-  def generate_code_from_name
-    return if name.blank?
-
-    # Vietnamese character mapping
-    vietnamese_map = {
-      "à" => "a", "á" => "a", "ả" => "a", "ã" => "a", "ạ" => "a",
-      "ă" => "a", "ằ" => "a", "ắ" => "a", "ẳ" => "a", "ẵ" => "a", "ặ" => "a",
-      "â" => "a", "ầ" => "a", "ấ" => "a", "ẩ" => "a", "ẫ" => "a", "ậ" => "a",
-      "đ" => "d",
-      "è" => "e", "é" => "e", "ẻ" => "e", "ẽ" => "e", "ẹ" => "e",
-      "ê" => "e", "ề" => "e", "ế" => "e", "ể" => "e", "ễ" => "e", "ệ" => "e",
-      "ì" => "i", "í" => "i", "ỉ" => "i", "ĩ" => "i", "ị" => "i",
-      "ò" => "o", "ó" => "o", "ỏ" => "o", "õ" => "o", "ọ" => "o",
-      "ô" => "o", "ồ" => "o", "ố" => "o", "ổ" => "o", "ỗ" => "o", "ộ" => "o",
-      "ơ" => "o", "ờ" => "o", "ớ" => "o", "ở" => "o", "ỡ" => "o", "ợ" => "o",
-      "ù" => "u", "ú" => "u", "ủ" => "u", "ũ" => "u", "ụ" => "u",
-      "ư" => "u", "ừ" => "u", "ứ" => "u", "ử" => "u", "ữ" => "u", "ự" => "u",
-      "ỳ" => "y", "ý" => "y", "ỷ" => "y", "ỹ" => "y", "ỵ" => "y"
-    }
-
-    # Convert to ASCII, uppercase, replace spaces with underscores
-    result = name.downcase
-    vietnamese_map.each { |vn, ascii| result = result.gsub(vn, ascii) }
-    result = result.gsub(/[^a-z0-9\s]/, "").gsub(/\s+/, "_").upcase
-
-    self.code = result
   end
 end
